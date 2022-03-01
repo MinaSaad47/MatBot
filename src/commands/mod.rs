440391022d6942caad2f8732ffd26b153e3195f9
MinaSaad47 <MinaSaad-47@outlook.com>
2022-03-01 09:55:11 +0200

    [Refactor] moved application commands to 'src/commands/cmds.rs'

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,2 +1,3 @@
 #[macro_use]
 pub mod utils;
+pub mod cmds;
--- src/event_handler.rs
+++ src/event_handler.rs
@@ -6,12 +6,9 @@ use serenity::{
         channel::Message,
         gateway::Ready,
         interactions::{
-            application_command::{
-                ApplicationCommand,
-                ApplicationCommandOptionType,
-            },
-            Interaction,
-            InteractionResponseType,
+        application_command::ApplicationCommand,
+        Interaction,
+        InteractionResponseType,
         },
     },
     prelude::*,
@@ -20,7 +17,10 @@ use serenity::{
 #[allow(unused_imports)]
 use crate::{
     config::Config,
-    commands::utils::*,
+    commands::{
+        utils::*,
+        cmds::*,
+    },
 };
 
 pub struct Handler;
@@ -61,82 +61,19 @@ impl EventHandler for Handler {
         info!("{} is connected", rdy.user.name);
 
         let conf = Config::from_json_file("settings.json").unwrap();
+        let material_types = conf.material_types.iter()
+            .map(|(material, _)| {
+                info!("found '{}' material type", material);
+                material.clone()
+            }).collect();
         let commands =
             ApplicationCommand::set_global_application_commands(&ctx.http,
                                                                 |cmds| {
-                cmds.create_application_command(|cmd| {
-                    cmd.name("version").description("Display MatBot version")
-                })
-                .create_application_command(|cmd| {
-                    cmd.name("display")
-                        .description("get material resources of a certain type")
-                        .create_option(|opt| {
-                            opt
-                                .name("material")
-                                .description("choose a material type")
-                                .kind(ApplicationCommandOptionType::String)
-                                .required(true);
-                            for (material, _) in &conf.material_types {
-                                info!("regisering '{}' choice", material);
-                                opt.add_string_choice(material, material);
-                            }
-                            opt
-                        })
-                })
-                .create_application_command(|cmd| {
-                    cmd.name("update")
-                        .description("update existing material type")
-                        .create_option(|opt| {
-                            opt
-                                .name("method")
-                                .description("a method to apply to a type")
-                                .kind(ApplicationCommandOptionType::SubCommandGroup)
-                                .create_sub_option(|sub_cmd| {
-                                    sub_cmd.name("add")
-                                        .description("add a resource to the type")
-                                        .kind(ApplicationCommandOptionType::SubCommand)
-                                        .create_sub_option(|sub_opt| {
-                                            sub_opt.name("material")
-                                                .required(true)
-                                                .description("choose a material type")
-                                                .kind(ApplicationCommandOptionType::String);
-                                            for (material, _) in &conf.material_types {
-                                                info!("regisering '{}' choice", material);
-                                                sub_opt.add_string_choice(material, material);
-                                            }
-                                            sub_opt
-                                        })
-                                        .create_sub_option(|name_value| {
-                                            name_value.name("name")
-                                                .required(true)
-                                                .description("Enter a new resource name")
-                                                .kind(ApplicationCommandOptionType::String)
-                                        })
-                                        .create_sub_option(|url_value| {
-                                            url_value.name("url")
-                                                .required(true)
-                                                .description("Enter a new reource url")
-                                                .kind(ApplicationCommandOptionType::String)
-                                        })
-                                })
-                                .create_sub_option(|sub_cmd| {
-                                    sub_cmd.name("delete")
-                                        .description("delete a resource from type")
-                                        .kind(ApplicationCommandOptionType::SubCommand)
-                                        .create_sub_option(|sub_opt| {
-                                            sub_opt.name("material")
-                                                .required(true)
-                                                .description("choose a material type")
-                                                .kind(ApplicationCommandOptionType::String);
-                                            for (material, _) in &conf.material_types {
-                                                info!("regisering '{}' choice", material);
-                                                sub_opt.add_string_choice(material, material);
-                                            }
-                                            sub_opt
-                                        })
-                                })
-                        })
-                })
+                cmds.set_application_commands(vec![
+                    version_app_cmd(),
+                    display_app_cmd(&material_types),
+                    update_app_cmd(&material_types),
+                ])
             }).await;
 
         debug!("set_global_application_commands():\n{:?}", commands);
